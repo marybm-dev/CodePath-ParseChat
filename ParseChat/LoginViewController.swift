b1da7af96463bc4bf4f347fb9c84fadd699a5859
Mary Martinez <mlmartinez85@gmail.com>
2016-10-25 17:36:25 -0700

    Setup simple loginView

--- ParseChat/LoginViewController.swift
+++ ParseChat/LoginViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
